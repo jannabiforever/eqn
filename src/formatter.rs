f4d6faa376a988ae9bbcd09f8fda39abb483d928
Jungin Yu <51292893+jannabiforever@users.noreply.github.com>
2026-09-03 21:47:18 +0900

    feat: expression trait for easy substitution (#6)
    * feat: expression trait for easy substitution
    
    * feat: expression trait require From<Symbol>

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,20 +1,23 @@
+use crate::set::Set;
+use crate::symbol::Symbol;
+
 pub trait Formatter {
-    type Expr;
+    type Expr: Expression;
 
     fn format_expr(&self, expr: Self::Expr) -> Self::Expr;
 }
 
-pub struct TrivialFormatter<E> {
+pub struct TrivialFormatter<E: Expression> {
     _marker: std::marker::PhantomData<E>,
 }
 
-impl<E> TrivialFormatter<E> {
+impl<E: Expression> TrivialFormatter<E> {
     pub fn new() -> Self {
         Self::default()
     }
 }
 
-impl<E> Default for TrivialFormatter<E> {
+impl<E: Expression> Default for TrivialFormatter<E> {
     fn default() -> Self {
         Self {
             _marker: std::marker::PhantomData,
@@ -22,10 +25,21 @@ impl<E> Default for TrivialFormatter<E> {
     }
 }
 
-impl<E> Formatter for TrivialFormatter<E> {
+impl<E: Expression> Formatter for TrivialFormatter<E> {
     type Expr = E;
 
     fn format_expr(&self, expr: Self::Expr) -> Self::Expr {
         expr
+    }
+}
+
+pub trait Expression: Clone + From<Symbol<Self::Domain>> {
+    type Domain: Set;
+
+    fn degrees_of_freedom(&self) -> usize;
+    fn substitute(&mut self, sym: Symbol<Self::Domain>, expr: &Self);
+    fn substituted(mut self, sym: Symbol<Self::Domain>, expr: &Self) -> Self {
+        self.substitute(sym, expr);
+        self
     }
 }
