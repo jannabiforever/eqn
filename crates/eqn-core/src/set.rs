4d6c2c27dec298e3f05dfcc8f0f67d4c6377fbfa
jannabiforever <jungini1226@gmail.com>
2026-09-05 11:43:22 +0900

    fix: remove NaturalNumberSet

--- crates/eqn-core/src/set.rs
+++ crates/eqn-core/src/set.rs
@@ -8,14 +8,3 @@ pub trait Set {
     // Ord gives expressions a total order for canonical (sorted) forms.
     type Element: Clone + Eq + std::fmt::Debug;
 }
-
-// ================================================================================
-// Domains
-// ================================================================================
-
-pub struct NaturalNumberSet;
-
-impl Set for NaturalNumberSet {
-    // TODO: use big-number like python so large numbers don't overflow
-    type Element = u64;
-}
