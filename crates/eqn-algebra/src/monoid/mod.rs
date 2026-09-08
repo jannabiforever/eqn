4fa1b9bd1a3a6aafff8e47ba8074e5e408497467
jannabiforever <jungini1226@gmail.com>
2026-09-08 18:33:06 +0900

    feat: pratt parsing generalizing (wip)

--- crates/eqn-algebra/src/monoid/mod.rs
+++ crates/eqn-algebra/src/monoid/mod.rs
@@ -1,9 +1,14 @@
+use std::str::FromStr;
+
 use eqn_core::op::{Associative, BinaryOperator, Identity};
 use eqn_core::rewriter::Expression;
 use eqn_core::set::{Elem, Set};
 use eqn_core::symbol::Symbol;
 
+pub mod parser;
 mod rewriter;
+
+use eqn_parser::Parser;
 // Re-exports
 pub use rewriter::{CommutativeMonoidRewriter, NonCommutativeMonoidRewriter};
 
@@ -50,6 +55,33 @@ pub enum MonoidExpr<M: Monoid> {
     Op(Vec<MonoidExpr<M>>),
 }
 
+impl<M: Monoid> MonoidExpr<M> {
+    /// Wraps a domain element as a constant expression.
+    #[inline]
+    pub const fn constant(value: <M::Domain as Set>::Element) -> Self {
+        Self::Const(value)
+    }
+
+    pub fn apply(self, rhs: Self) -> Self {
+        match (self, rhs) {
+            (Self::Op(mut lhs), Self::Op(rhs)) => {
+                lhs.extend(rhs);
+                Self::Op(lhs)
+            }
+            (Self::Op(mut lhs), rhs) => {
+                lhs.push(rhs);
+                Self::Op(lhs)
+            }
+            (lhs, Self::Op(rhs)) => {
+                let mut v = vec![lhs];
+                v.extend(rhs);
+                Self::Op(v)
+            }
+            (lhs, rhs) => Self::Op(vec![lhs, rhs]),
+        }
+    }
+}
+
 impl<M: Monoid> Expression for MonoidExpr<M> {
     type Domain = M::Domain;
 
@@ -72,14 +104,6 @@ impl<M: Monoid> Expression for MonoidExpr<M> {
             Self::Symbol(s) => Some(s),
             _ => None,
         }
-    }
-}
-
-impl<M: Monoid> MonoidExpr<M> {
-    /// Wraps a domain element as a constant expression.
-    #[inline]
-    pub const fn constant(value: MonoidElem<M>) -> Self {
-        Self::Const(value)
     }
 }
 
@@ -119,5 +143,18 @@ mod tests {
                 )));
             }
         }
+    }
+}
+
+impl<M> FromStr for MonoidExpr<M>
+where
+    M: Monoid,
+{
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let token_iterator = parser::MonoidExprTokenizer::new(s);
+        let parser = parser::MonoidParser::new(token_iterator);
+        parser.parse()
     }
 }
